82bdf22064979101d23645fcdc9b218eea562732
unset <unset@gmail.com>
2024-09-17 14:00:57 -0300

    LED Toggle Implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import logo from './logo.svg';
+
+import {Button, Form} from "react-bootstrap";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCoffee} from "@fortawesome/free-solid-svg-icons";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ledValue, setLedValue] = useState('off')
+
+    const formSubm = function () {
+        fetch('http://192.168.1.145/?led='+ledValue, {
+            method: 'POST',
+            mode: 'no-cors',
+            body: 'on'
+        })
+
+        if (ledValue === 'on') {
+            setLedValue('off')
+        } else {
+            setLedValue('on')
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Edit <code>src/App.js</code> and save to reload.</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <p>{ledValue}</p>
+            <Button onClick={formSubm}
+                name={"led"}
+                value={ledValue}
+            >
+                <FontAwesomeIcon icon={faCoffee} />
+            </Button>
+        </div>
+    );
 }
 
 export default App;
